12f1c9219aaa1df57a2e70a119e746710842e03d
Maxim Villivald <mv@MacBook-Pro-Maxim.local>
2020-03-19 14:41:38 +0200

    calculator

--- Muut/part1/src/index.js
+++ Muut/part1/src/index.js
@@ -2,9 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }
+
     return (
       <div>
+      <p>
         Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born {bornYear()}</p>
       </div>
     )
   }
